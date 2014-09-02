f78191c02562b8d0d686864e7974f816158ce761
Reiko Randoja <reiko.randoja@gmail.com>
2014-09-02 19:27:46 +0300

    Code using gyro to hold angle

---
+++ tellis2.js
@@ -0,0 +1,166 @@
+var express = require('express'),
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server),
+    dgram = require('dgram'),
+    client = dgram.createSocket('udp4'),
+    serverPort = 8042,
+    serverAddress = '192.168.4.1',
+	angle = 0,
+	angleSetpoint = 0;
+
+var pidFreq = 60,
+    ticksPerRev = 1200,
+    wheelCirc = 0.07 * Math.PI,
+    wheelOffset = 0.117,
+    metricToRobot = ticksPerRev / wheelCirc / pidFreq,
+    maxSpeed = 2,
+    maxRotation = 4,
+    wheel1Angle = 135 / 180 * Math.PI,
+    wheel2Angle = 45 / 180 * Math.PI,
+    wheel3Angle = 225 / 180 * Math.PI,
+    wheel4Angle = 315 / 180 * Math.PI,
+    xSpeed = 0,
+    ySpeed = 0,
+    rotation = 0,
+	wheel1Speed = 0,
+	wheel2Speed = 0,
+	wheel3Speed = 0,
+	wheel4Speed = 0,
+	dribblerSpeed = 0;
+
+var lastSpeed = 0,
+	lastAngle = 0,
+	lastRotation = 0;
+
+app.configure(function () {
+    app.use(express.static(__dirname + '/public'));
+});
+
+server.listen(8083);
+
+io.set('log level', 2);
+
+io.sockets.on('connection', function (socket) {
+    socket.on('drive', function (data) {
+        data = data || {};
+        //drive(data.speed || 0, data.angle || 0, data.rotation || 0);
+		drive(data.speed || 0, data.angle || 0, lastRotation || 0);
+    });
+
+    socket.on('dribbler', function (data) {
+        data = data || {};
+        dribbler(data.speed || 0);
+    });
+
+    socket.on('kick', function (data) {
+        var strength = data.strength || 500;
+        kick(strength);
+    });
+
+	socket.on('set angle', function (newAngle) {
+        angleSetpoint = newAngle;
+    });
+});
+
+client.on("message", function (msg, rinfo) {
+	var angleRegex = /<angle:(.*)>/;
+	var match = angleRegex.exec(msg);
+	if (match) {
+		angle = parseFloat(match[1]);
+		io.sockets.emit('angle', angle);
+		//console.log(angle);
+	}
+	//console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
+});
+
+var error = 0,
+	errorIntegral = 0,
+	maxRotSpeed = 20,
+	pGain = 5,
+	iGain = 0.01,
+	maxI = maxRotSpeed / iGain,
+	minRotSpeed = 0;
+
+setInterval(function() {
+	error = (angle - angleSetpoint) / 180 * Math.PI;
+	errorIntegral += error
+	if (errorIntegral >= maxI) {
+		errorIntegral = maxI;
+	} else if (errorIntegral <= -maxI) {
+		errorIntegral = -maxI;
+	}
+
+	console.log(Math.abs(error));
+
+	if (Math.abs(error) <= 0.05)	{
+		error = 0;
+		errorIntegral = 0;
+	}
+
+	var rotSpeed = pGain * error + iGain * errorIntegral;
+	//var rotSpeed = 0.5 * error;
+
+	if (rotSpeed >= maxRotSpeed) {
+		rotSpeed = maxRotSpeed;
+	} else if (rotSpeed <= -maxRotSpeed) {
+		rotSpeed = -maxRotSpeed;
+	}
+
+	//console.log(rotSpeed);
+
+	if (Math.abs(rotSpeed) >= minRotSpeed) {
+		drive(lastSpeed, lastAngle, rotSpeed);
+	}	
+
+	var message = new Buffer('ga');
+    client.send(message, 0, message.length, serverPort, serverAddress);
+}, 1 / 50);
+
+function drive(speed, angle, rotation) {
+	lastSpeed = speed;
+	lastAngle = angle;
+	lastRotation = rotation;
+    var rotationalSpeed = speedMetricToRobot(rotationRadiansToMetersPerSecond(rotation));
+    wheel1Speed = Math.round(speedMetricToRobot(wheelSpeed(speed, angle, wheel1Angle)) + rotationalSpeed);
+    wheel2Speed = Math.round(speedMetricToRobot(wheelSpeed(speed, angle, wheel2Angle)) + rotationalSpeed);
+    wheel3Speed = Math.round(speedMetricToRobot(wheelSpeed(speed, angle, wheel3Angle)) + rotationalSpeed);
+    wheel4Speed = Math.round(speedMetricToRobot(wheelSpeed(speed, angle, wheel4Angle)) + rotationalSpeed);
+    speeds = [wheel1Speed, wheel2Speed, wheel3Speed, wheel4Speed];
+    console.log(wheel1Speed, wheel2Speed, wheel3Speed, wheel4Speed);
+    var message = new Buffer('speeds:' + wheel1Speed + ':' + wheel2Speed + ':' + wheel3Speed + ':' + wheel4Speed + ':' + dribblerSpeed);
+    client.send(message, 0, message.length, serverPort, serverAddress);
+}
+
+function dribbler(speed) {
+    dribblerSpeed = Math.round(speed);
+    if (dribblerSpeed < -255) dribblerSpeed = 255;
+    else if (dribblerSpeed > 255) dribblerSpeed = 255;
+    console.log('Dribbler:', dribblerSpeed);
+	var message = new Buffer('speeds:' + wheel1Speed + ':' + wheel2Speed + ':' + wheel3Speed + ':' + wheel4Speed + ':' + dribblerSpeed);
+    client.send(message, 0, message.length, serverPort, serverAddress);
+}
+
+function kick(strength) {
+    strength = strength || 500;
+    console.log('Kick', strength);
+    var message = new Buffer('kick:' + strength);
+    client.send(message, 0, message.length, serverPort, serverAddress);
+}
+
+function wheelSpeed(robotSpeed, robotAngle, wheelAngle) {
+    return robotSpeed * Math.cos(wheelAngle - robotAngle);
+}
+
+function speedMetricToRobot(metersPerSecond) {
+    return metersPerSecond * metricToRobot;
+}
+
+function speedRobotToMetric(wheelSpeed) {
+    return wheelSpeed / metricToRobot;
+}
+
+function rotationRadiansToMetersPerSecond(radiansPerSecond) {
+    return radiansPerSecond * wheelOffset;
+}
